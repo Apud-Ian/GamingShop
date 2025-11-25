7ce913114760a185d0678a76f73e4de523fb86d9
ian apud <ianapud23@gmail.com>
2025-11-25 20:46:51 -0300

    inicializando el proyecto

---
+++ GamingShop/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">Welcome to the Gaming Shop</h1>
+      {/* Additional components and routes will go here */}
+    </div>
+  );
+}
+export default App;
